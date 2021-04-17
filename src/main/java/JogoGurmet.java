64420100138eafd04d2da6c888ac00d7af1b06d4
Marcus Montanher <marcusmontanher@hotmail.com>
2021-04-17 17:39:05 -0300

    Gerando jar com dependencias

--- src/main/java/JogoGurmet.java
+++ src/main/java/JogoGurmet.java
@@ -1,5 +1,4 @@
 
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
@@ -7,7 +6,6 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
